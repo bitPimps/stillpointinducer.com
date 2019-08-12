0f26174c98ede7a2831eba8a61729267ea84c5d9
bitPimps <bitpimps@gmail.com>
2019-08-12 13:04:14 -0500

    Initial import

---
+++ wwwRoot/index.php
@@ -0,0 +1,84 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Still Point Inducer&trade;</title>
+		<meta name="description" content="">
+		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" rel="stylesheet">
+		<link href="css/main.css" rel="stylesheet">
+		<!--[if lt IE 9]>
+			<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+			<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+	<div class="jumbotron">
+		<div class="container ui-logo">
+			<h1>Still Point Inducer&trade;</h1>
+			<p>by The Upledger Institute International, Inc<sup>&reg;</sup></p>
+			<p><a href="http://shop.iahe.com/Still-Point-Inducer" class="btn btn-success btn-lg" role="button">Purchase Now! &raquo;</a></p>
+		</div>
+	</div>
+	<div class="container">
+		<h2>Relax In One Simple Step</h2>
+		<div class="row">
+			<div class="col-sm-8">
+				<h3>Inducing a Still Point may help in:</h3>
+				<ul>
+					<li>Providing deep relaxation and helping reduce stress</li>
+					<li>Relieving headaches</li>
+					<li>Easing chronic musculoskeletal pain</li>
+					<li>Enhancing immune system efficiency</li>
+					<li>Facilitating your body's self-correcting abilities</li>
+					<li>Promoting an overall sense of well-being</li>
+				</ul>
+				<p>This Still Point Inducer&trade; was inspired by Osteopathic Physician John E. Upledger, developer of CranioSacral Therapy. A Still Point is a pause in the craniosacral rhythm and may have many health benefits.</p>
+				<p>In keeping with our green initiative, the Upledger Institute International is now offering the Upledger Still Point Inducer&trade; in a recyclable, easy-to- open carton.</p>
+				<p>New to the package is a CD featuring healing music performed by an accomplished CST practitioner to encourage deep relaxation.</p>
+				<p>Dimensions: 6&quot; Wide x 7 3/8&quot; High x 2 1/2&quot; Deep</p>
+				<hr>
+				<div class="embed-responsive embed-responsive-16by9">
+					<iframe class="embed-responsive-item" id="vzvd-2844576" name="vzvd-2844576" src="//video.iahe.com/2844576/player"></iframe>
+				</div>
+			</div>
+			<div class="col-sm-4">
+				<div class="panel panel-info">
+					<div class="panel-heading">Order Directly From the Source!</div>
+					<div class="panel-body">
+						<div class="thumbnail">
+							<a href="http://shop.iahe.com/Still-Point-Inducer"><img src="https://system.netsuite.com/core/media/media.nl?id=262543&c=698760&h=e77e16e6728a2dceb4e6" class="img-rounded"></a>
+						</div>
+						<p><a href="http://shop.iahe.com/Still-Point-Inducer" class="btn btn-success btn-lg" role="button">Purchase Online &raquo;</a></p>
+						<p><strong>Or</strong></p>
+						<h4>Call 1.800.233.5880 to order by phone</h4>
+						<p><em>(And ask about receiving quantity discounts)</em></p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<hr>
+		<p><strong>For more information about The Still Point Inducer or The Upledger Institute and Clinic, please visit us online at:</strong></p>
+		<p><a href="https://www.upledger.com/" class="btn btn-info btn-lg" role="button">upledger.com</a></p>
+		<hr>
+		<footer>
+			<p class="text-muted"><small>&copy; <?php echo date("Y"); ?> The Upledger Institute International, Inc.<sup>&reg;</sup></small></p>
+		</footer>
+	</div><!-- /container -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-10031363-4"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+
+		gtag('config', 'UA-10031363-4');
+	</script>
+	</body>
+</html>
