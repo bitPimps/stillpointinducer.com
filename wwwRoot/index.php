92f108a6768e68103ca0d75137ba5ebb4ac95bc0
bitPimps <bitpimps@gmail.com>
2019-10-15 14:37:14 -0500

    Update for new server
    Updating files for new server.

--- wwwRoot/index.php
+++ wwwRoot/index.php
@@ -11,7 +11,7 @@
 		<meta name="description" content="">
 		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
 		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" rel="stylesheet">
-		<link href="css/main.css" rel="stylesheet">
+		<link href="/css/main.css" rel="stylesheet">
 		<!--[if lt IE 9]>
 			<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
 			<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
